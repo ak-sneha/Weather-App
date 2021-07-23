645e2f94b854989e2da14254f2f8211dff7a7937
sbangade <sneha.bangade@nokia.com>
2021-07-23 19:22:02 +0530

    task-9 implemented main screen ui.

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weatherapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var animationDrawable: AnimationDrawable
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.mainRoot)
+
+        supportActionBar?.hide()
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        animationDrawable = binding.root.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(5000)
+        animationDrawable.setExitFadeDuration(2000)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        animationDrawable.start()
+
     }
 }
